6aa316f38df06086c7de1b124b73513c4d27c959
TennysonKnoxLove <TennysonKnoxLove@gmail.com>
2025-06-04 04:29:00 -0400

    Responsive UI adjustments

--- tennyson-portfolio/src/app/shell/navbar/navbar.component.ts
+++ tennyson-portfolio/src/app/shell/navbar/navbar.component.ts
@@ -1,20 +1,28 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
   imports: [
+    CommonModule,
     MatToolbarModule,
     MatButtonModule,
+    MatIconModule,
     RouterLink,
     RouterLinkActive
   ],
   standalone: true
 })
 export class NavbarComponent {
+  isMobileMenuOpen = false;
 
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
 }
